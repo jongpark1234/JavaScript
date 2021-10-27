45ca41a3d8efaa7530b3681b1bda18f3ea9b2b93
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-27 20:19:16 +0900

    Basic, Variable, Constant

--- JsTraining001.js
+++ JsTraining001.js
@@ -1,1 +1,19 @@
 console.log('Hello, JavaScript!')
+
+// variable and constant
+
+let value = 1 // let <name> = <value> : variable declaration
+console.log(value) // 1
+value = 2
+console.log(value) // 2
+
+// let value = 1
+// let value = 2 
+// ==> Error ( Syntax Error )
+
+const a = 1 // const <name> = <value> : constant declaration
+// a = 2 ==> Eror ( "a" is read-only )
+
+// const value = 1
+// const value = 2 
+// ==> Error ( Syntax Error )
