bdbd0a534f6faab48a4a4e1094b27225e76a2905
종박 <78944379+jongpark1234@users.noreply.github.com>
2021-10-27 20:34:56 +0900

    Number, String, Boolean, null, defined Data Type
    2021_10_27

---
+++ JsTraining002.js
@@ -0,0 +1,31 @@
+// Data Type
+
+// Number Data Type
+
+let value = 1 // Substituting 1 into the value.
+
+// String Data Type
+
+let text = 'Hello'
+let language = 'JavaScript'
+
+console.log(text, language) // Hello JavaScript
+
+// Subsituting 'Hello' and 'JavaScript' into the text and language.
+
+// Boolean Data Type
+
+let bool1 = true
+let bool2 = false
+console.log(bool1, bool2) // true false
+console.log(Number(bool1), Number(bool2)) // 1 0
+
+// null and undefined
+
+// null : There's no value for this.
+// undefined : This value was not declared.
+
+const A = null
+let B
+console.log(A) // null
+console.log(B) // undefined
